f9daa0b218f09aa413a15fce627b7dfa810d24f9
Navyashreekn22 <104260237+Navyashreekn22@users.noreply.github.com>
2023-07-08 16:06:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let result = document.getElementById('result');
+
+function appendNumber(number) {
+  result.value += number;
+}
+
+function appendOperator(operator) {
+  result.value += operator;
+}
+
+function calculateResult() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = 'Error';
+  }
+}
+
+function clearResult() {
+  result.value = '';
+}
